351e8a971ca8fdc43a881b84b0d8ac5d73d1935d
Munkhtamir <munkhtamir.g@outlook.com>
2023-04-06 08:55:40 +0800

    [ADD] server

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
+import { getContrastingColor } from "../config/helpers";
 
 const CustomButton = ({ title, customStyles, type, handleClick }) => {
   const snap = useSnapshot(state);
@@ -10,8 +11,10 @@ const CustomButton = ({ title, customStyles, type, handleClick }) => {
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "fff",
+        color: getContrastingColor(snap.color),
       };
+    } else if (type === "outline") {
+      return { borderWidth: "1px", borderColor: snap.color, color: snap.color };
     }
   };
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -14,7 +14,7 @@ import {
   FilePicker,
   Tab,
 } from "../components";
-import { EditorTabs, FilterTabs } from "../config/constants";
+import { DecalTypes, EditorTabs, FilterTabs } from "../config/constants";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
@@ -36,12 +36,81 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />;
       case "filepicker":
-        return <FilePicker />;
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generateingImg={generateingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
     }
+  };
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+      setGenerateingImg(true);
+
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGenerateingImg(false);
+      setActiveEditorTab("");
+    }
+  };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+    }
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      };
+    });
+  };
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
   };
 
   return (
@@ -89,8 +158,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
